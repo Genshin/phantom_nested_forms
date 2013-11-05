98fdbbc0e64e5f9c3387079ba84aa6fd888deefe
Vassil Kalkov <vassilkalkov@gmail.com>
2013-11-05 11:40:55 +0200

    Fix modal_nested_form_for

--- lib/phantom_nested_forms/helper.rb
+++ lib/phantom_nested_forms/helper.rb
@@ -18,7 +18,17 @@ module PhantomNestedForms
       options[:builder] = PhantomNestedForms::FormBuilders::ValidateNestedFormBuilder
       options[:html] = {:'data-type' => 'script', :class => 'remote-form'}
       options[:remote] = true
-      nested_form_for(object, options, &block)
+
+      object_name = get_class(extract_object(object))
+      object_class = options[:resource] ||  object_name
+      label = options[:label] || t("#{object_name.underscore}.singular")
+
+      content_tag :div, class: "col-md-12 alert-dismissable" do
+        content_tag :div, class: "panel panel-primary" do
+          concat(panel_title(label, modal_close_button))
+          concat(content_tag(:div, class: "panel-body") { nested_form_for(object, options, &block) })
+        end
+      end
     end
 
   end
